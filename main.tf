65ec120191ae840bee30993f7c559a90024ee825
vsnandy <3279133+vsnandy@users.noreply.github.com>
2024-09-28 02:08:05 -0500

    tf

--- main.tf
+++ main.tf
@@ -18,12 +18,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "vsnandy-tfstate"
-     
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
