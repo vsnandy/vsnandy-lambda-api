3430b7cf2518acf4a339482a51d5b4d2ec1f9a40
vsnandy <3279133+vsnandy@users.noreply.github.com>
2024-09-27 20:31:37 -0500

    Deploy hello world

--- src/handler.py
+++ src/handler.py
@@ -1,8 +1,23 @@
+import os
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def handler(event, context):
-    # TODO: Implement
+    # TODO: vsnandy-lambda-api
+    logger.info()
+    logger.info("*** ENVIRONMENT VARIABLES ***")
+    logger.info(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
+    logger.info(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
+    logger.info('*** EVENT ***')
+    logger.info(event)
+
+    message = f"Hello, {event['name']}!"
+
+
     return {
         'statusCode': 200,
-        'body': json.dumps("Hello, World!")
+        'body': json.dumps(message)
     }
