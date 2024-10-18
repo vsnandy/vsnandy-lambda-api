4cf70ac34664cb35cbc7b03176fda8875e61e848
vsnandy <3279133+vsnandy@users.noreply.github.com>
2024-10-18 13:06:23 -0500

    Adding configs for user pool

--- main.tf
+++ main.tf
@@ -221,6 +221,65 @@ resource "aws_dynamodb_table" "vsnandy_db" {
 // COGNITO RESOURCES
 resource "aws_cognito_user_pool" "pool" {
   name = "vsnandy-users"
+
+  mfa_configuration = "ON"
+  username_attributes = ["email"]
+  deletion_protection = "ACTIVE"
+  auto_verified_attributes = ["email"]
+  tags = {}
+  tags_all = {}
+  account_recovery_setting {
+    recovery_mechanism {
+      name = "verified_email"
+      priority = 1
+    }
+  }
+
+  admin_create_user_config {
+    allow_admin_create_user_only = true
+  }
+
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
+
+  password_policy {
+    minimum_length = 8
+    password_history_size = 0
+    require_lowercase = true
+    require_numbers = true
+    require_symbols = true
+    require_uppercase = true
+    temporary_password_validity_days = 7
+  }
+
+  schema {
+    attribute_data_type = "String"
+    developer_only_attribute = false
+    mutable = true
+    name = "email"
+    required = true
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
+    }
+  }
+
+  software_token_mfa_configuration {
+    enabled = true
+  }
+
+  user_attribute_update_settings {
+    attributes_require_verification_before_update = ["email"]
+  }
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+  }
 }
 
 resource "aws_cognito_user_pool_client" "client" {
